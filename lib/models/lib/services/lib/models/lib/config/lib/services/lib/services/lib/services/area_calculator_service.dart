26b9479379e1e686f1f584de861161583aa42f51
poltroniericorreiam-code <poltroniericorreiam@gmail.com>
2026-06-04 19:47:53 -0400

    Add AreaCalculatorService for area conversions

---
+++ lib/models/lib/services/lib/models/lib/config/lib/services/lib/services/lib/services/area_calculator_service.dart
@@ -0,0 +1,9 @@
+class AreaCalculatorService {
+  double hectaresToKm2(double hectares) {
+    return hectares / 100;
+  }
+
+  double hectaresToAlqueirePaulista(double hectares) {
+    return hectares / 2.42;
+  }
+}
